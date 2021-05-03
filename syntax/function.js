3eab77667138cd5dab744867a88971dce1011bfb
nidoos <hsoobin21@gmail.com>
2021-05-03 15:43:16 +0900

    function

---
+++ syntax/function.js
@@ -0,0 +1,22 @@
+f123();
+console.log('A');
+console.log('B');
+console.log('C');
+f123();
+console.log('d');
+console.log('e');
+console.log('f');
+f123();
+console.log('g');
+console.log('h');
+console.log('i');
+f123();
+
+
+
+function f123(){
+    console.log(1);
+    console.log(2);
+    console.log(3);
+
+}
